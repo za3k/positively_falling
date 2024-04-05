8cb3f8e7108b52ec9bc1bfdb59d7256c8b2d535d
Zachary Vance <za3k@za3k.com>
2024-04-05 16:28:34 -0400

    Add inactive magnets

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var x = 0.5
 var alive = true
 var polarity = true
 
+function randomBool() { return Math.random() < 0.5 }
+
 function xToPixels(x) {
     // (0, 1) to (0, width)
     return x*width
@@ -30,9 +32,29 @@ function showDepth(now) {
 
 function getLeftWall(d) { return Math.sin(d/3)/8+0.25 } // 0 t
 function getRightWall(d) { return Math.sin(d/3)/8+0.75 }
+var allMagnets = {}
+function randomMagnet(depth) {
+    return {
+        depth: depth,
+        side: randomBool(),
+        polarity: randomBool()
+    }
+}
+function nthMagnet(i, depth) {
+    if (!allMagnets[i]) allMagnets[i] = randomMagnet(depth)
+    return allMagnets[i]
+}
+function getMagnets(d1, d2) {
+    var magnets = []
+    var spacing = 10
+    for (i=Math.ceil(d1/spacing); i<d2/spacing; i++) {
+        magnets.push(nthMagnet(i, i*spacing));
+    }
+    return magnets
+}
 function playerDead() { return !alive }
 function drawWall(wallPos, d1, d2) {
-    for (var pixel = 0; pixel<height; pixel++) {
+    for (var pixel=0; pixel<height; pixel++) {
         var percent = pixel / height
         var depth = d1 + (percent * (d2-d1))
         lx = xToPixels(wallPos(depth))
@@ -59,7 +81,26 @@ function showLevelWalls(d1, d2) {
     ctx.lineTo(width, 0)
     ctx.fill()
 }
-function showLevelMagnets(d1, d2) {}
+function drawMagnet(x, y, polarity) {
+    console.log("showPlayer")
+    if (polarity) {
+        ctx.fillStyle="red";
+    } else {
+        ctx.fillStyle = "blue";
+    }
+    ctx.beginPath()
+    ctx.arc(x, y, 10, 0, 2*Math.PI)
+    ctx.fill()
+}
+function showLevelMagnets(d1, d2) {
+    for (magnet of getMagnets(d1, d2)) {
+        var percent = (magnet.depth - d1) / (d2-d1)
+        var yPixels = height*percent
+        var x = magnet.side ? getLeftWall(magnet.depth) : getRightWall(magnet.depth)
+        var xPixels = xToPixels(x)
+        drawMagnet(xPixels, yPixels, magnet.polarity)
+    }
+}
 function showPlayer() {
     console.log("showPlayer")
     if (polarity) {
@@ -103,3 +144,13 @@ window.addEventListener("load", () => {
     }, 20)
 })
 
+var spacePressed = false;
+document.addEventListener("keydown", (e) => {
+    if (e.key == " ") {
+        if (!spacePressed && alive) polarity = !polarity // Toggle polarity
+        spacePressed = true
+    }
+})
+document.addEventListener("keyup", (e) => {
+    if (e.key == " ") spacePressed = false
+})
